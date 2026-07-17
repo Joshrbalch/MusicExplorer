2e27768dbdd78b5a45403bb71567c2e08a0cf550
Josh Balch <Joshrbalch@gmail.com>
2026-07-17 13:23:13 -0500

    art size hotfix

--- src/app.tsx
+++ src/app.tsx
@@ -709,6 +709,7 @@ ${tracklistMarkdown}
                                         }}
                                     >
                                         <img 
+                                            key={cover}
                                             src={cover || "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%23888888' stroke-width='1' stroke-linecap='round' stroke-linejoin='round'%3E%3Crect x='3' y='3' width='18' height='18' rx='2' ry='2'%3E%3C/rect%3E%3Ccircle cx='8.5' cy='8.5' r='1.5'%3E%3C/circle%3E%3Cpath d='M21 15l-5-5L5 21'%3E%3C/path%3E%3C/svg%3E"} 
                                             alt="Cover" 
                                             style={{ 
